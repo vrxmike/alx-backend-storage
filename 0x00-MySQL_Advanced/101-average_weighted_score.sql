15829c3cc4e13703fd6fc60a1c3c078125725786
vrxmike <alvinvrxmike@icloud.com>
2023-09-13 03:36:42 +0300

    fix 101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -9,7 +9,7 @@ BEGIN
     (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
     FROM users AS U
     JOIN corrections as C ON U.id=C.user_id
-    JOIN projects AS P ON C.product_id=P.id
+    JOIN projects AS P ON C.project_id=P.id
     GROUP BY U.id)
   AS WA
   SET U.average_score = WA.w_avg
