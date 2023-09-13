5a89fb07e2ae87746088437212afeeb42330d25c
vrxmike <alvinvrxmike@icloud.com>
2023-09-13 03:27:14 +0300

    hotfix 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,7 +2,7 @@
 -- that computes and store the average score for a student
 drop procedure IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$ ;
-CREATE PROCEDURE ComputrAverageScoreForUser(
+CREATE PROCEDURE ComputeAverageScoreForUser(
         IN user_id INT
 )
 BEGIN
@@ -10,7 +10,6 @@ BEGIN
         SET average_score=(SELECT AVG(score) FROM corrections
                             WHERE corrections.user_id=user_id)
         WHERE id=user_id;
-
 
 END;$$
 DELIMITER ;
